0af55a4c4d3b9b8b1dcd12b849c054cc3a3fd972
JEY <jey.pley@gmail.com>
2024-02-16 13:27:12 +0100

    fetching listing with server components

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <div className="text-blue-500 text-2xl">Hello Airbnb!</div>;
+import getListings from "@/app/actions/getListings";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+
+import Container from "@/app/components/Container";
+import EmptyState from "@/app/components/EmptyState";
+import ListingCard from "@/app/components/listings/ListingCard";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) return <EmptyState showReset />;
+
+  return (
+    <Container>
+      <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
+        {listings.map((listing: any) => {
+          return (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          );
+        })}
+      </div>
+    </Container>
+  );
 }
